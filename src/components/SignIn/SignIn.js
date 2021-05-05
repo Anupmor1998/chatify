f3115dc455df4a210efe491c5fa23aced6bbd9b9
Anupmor1998 <53808559+Anupmor1998@users.noreply.github.com>
2021-05-05 16:02:49 +0530

    commit

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { auth, firebase } from "../../firebase/firebase";
+// import { auth, firebase } from "../../firebase/firebase";
 import google from "../../images/google.png";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 function SignIn() {
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
+    firebase.auth().signInWithPopup(provider);
   };
   return (
     <button className="sign-in" onClick={signInWithGoogle}>
